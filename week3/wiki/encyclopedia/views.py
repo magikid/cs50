b5db3022daa6261afefe8d317f7d698d864b467f
Chris W Jones <chris@christopherjones.us>
2024-09-28 15:49:36 -0400

    Adds tests and finishes up entry and index pages

--- week3/wiki/encyclopedia/views.py
+++ week3/wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import markdown2
 
 from . import util
 
@@ -8,3 +9,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def get_entry(request, title):
+    raw_entry = util.get_entry(title)
+
+    if raw_entry is None:
+        return render(request, "encyclopedia/error.html", {
+            "title": title
+        })
+
+    markdown = markdown2.markdown(raw_entry)
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": markdown,
+    })
